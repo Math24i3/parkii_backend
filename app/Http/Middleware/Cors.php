09f6387fd919a90947887c4df7e169cb6d742f09
Mathias M. Feldt <feldt.mathias@gmail.com>
2022-04-27 18:28:19 +0200

    Cors correction middleware fruitcake

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,9 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', '*');
+            ->header ("Access-Control-Expose-Headers: Content-Length, X-JSON")
+            ->header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS")
+            ->header ("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization")
+            ->header('Access-Control-Max-Age: 86400');
     }
 }
