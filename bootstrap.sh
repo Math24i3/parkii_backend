c43040c8274ff6d447dc218545c19eedc6b14573
Mathias M. Feldt <feldt.mathias@gmail.com>
2022-04-29 13:29:37 +0200

    Doppler integration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 cd /var/www
+php artisan optimize
+
 doppler secrets -p parkii-api -c prd download --no-file --format env > .env -t dp.st.prd.O5yoHNo8fSLbF3YKywo81JuHtEMKH1AWmP03loY6L2L
 echo 'APP_ID='$(uuidgen) >> .env
+
 php artisan config:clear
+
 php-fpm
