3b190f210cc8e1f43de70a0194a48d9f6540e3aa
Mathias M. Feldt <feldt.mathias@gmail.com>
2022-05-11 14:38:57 +0200

    Deploy script update

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 git pull
 docker-compose -f docker-compose.swarm.yml build
 docker-compose -f docker-compose.swarm.yml push
+docker service update --image 127.0.0.1:5000/parkii_backend_app api_app
 #docker network create api_default
 #docker stack deploy --compose-file docker-compose.swarm.yml api
-docker service update api_app
