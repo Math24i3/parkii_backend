1acf61e3845d73fd3a25cb6ece590477da6783d0
Mathias M. Feldt <feldt.mathias@gmail.com>
2022-05-11 14:50:41 +0200

    Updates to api status page

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #docker stack rm api
-docker service update --image 127.0.0.1:5000/parkii_backend_app api_app
+git pull
+docker-compose -f docker-compose.swarm.yml build
+docker-compose -f docker-compose.swarm.yml push
 #docker network create api_default
 #docker stack deploy --compose-file docker-compose.swarm.yml api
