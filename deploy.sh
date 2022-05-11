3f7ce12aa10cd4541c4bfdaffb1dddf8aae2ee83
Mathias M. Feldt <feldt.mathias@gmail.com>
2022-05-11 14:06:20 +0200

    Changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ docker stack rm api
 git pull
 docker-compose -f docker-compose.swarm.yml build
 docker-compose -f docker-compose.swarm.yml push
+docker network create api_default
 docker stack deploy --compose-file docker-compose.swarm.yml api
