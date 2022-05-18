3349ef6e7c3b63065aa08b6b5c012c1ab1f5e89a
Mathias M. Feldt <feldt.mathias@gmail.com>
2022-05-18 14:29:35 +0200

    Updates to api status page

--- app/Services/RestrictionService.php
+++ app/Services/RestrictionService.php
@@ -56,9 +56,7 @@ class RestrictionService
             case RestrictionContract::THREE_HOUR_RESTRICTION:
                 $format["rule"] = "Parking is restricted to 3 hours";
                 //$response['keywords'] = preg_split("~(?:\d+|\D+)\K~", $restriction["restriktionstekst"],0, PREG_SPLIT_NO_EMPTY);
-                if($restriction->restriktionstype === "Tidsbegrænsning"
-                    && isset($restriction->restriktionstekst)
-                    && $this->match('[0-9]+(?:\,|\-)[0-9]+', $restriction["restriktionstekst"])) {
+                if($restriction->restriktionstype === "Tidsbegrænsning" && $this->match('[0-9]+(?:\,|\-)[0-9]+', $restriction["restriktionstekst"])) {
                     $format["rule"] .= " between " . $restriction["restriktionstekst"];
                 }
 
